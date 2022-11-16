36ca36e908bb6a23e1343e876d36f0054002f08f
Yasmin Ramadini <75203699+yasminramadini@users.noreply.github.com>
2022-11-16 22:02:24 +0700

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,8 +4,7 @@ import ReactEmoji from "react-emoji";
 const Message = ({ message, userName }) => {
   return message.user !== userName ? (
     <div
-      className="bg-gray-700 mt-4 text-white p-3 rounded-md w-48"
-      style={{ width: "40%" }}
+      className="bg-gray-700 mt-4 text-white p-3 rounded-md w-48 message"
     >
       <p className="font-bold">{message.user}</p>
       <p className="mt-2">{ReactEmoji.emojify(message.message)}</p>
